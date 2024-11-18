f9b4326b6d57602ab696c50d414cba22fd5a14cc
nohyewon <yw4913@naver.com>
2024-11-18 19:28:29 +0900

    :sparkles:[feat]: 홈 초기 작성 및 stack 추가

---
+++ src/navigation/stack/HomeStack.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home, FavoriteHomes } from '../../screens/home/index';
+import { BackIcon } from '../../assets/icons/iconSvg';
+
+const Stack = createNativeStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleStyle: { fontSize: 24 },
+        headerShadowVisible: false,
+        headerBackVisible: false,
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: '홈',
+          headerTitleAlign: 'left',
+        }}
+      />
+      <Stack.Screen
+        name="FavoriteHomes"
+        component={FavoriteHomes}
+        options={({ navigation }) => ({
+          title: '내가 찜한 집',
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <View style={{ marginLeft: 10 }}>
+              <BackIcon onPress={() => navigation.goBack()} />
+            </View>
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default HomeStack;
