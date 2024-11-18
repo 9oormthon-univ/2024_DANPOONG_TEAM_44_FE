03a206c5b1a64d8cd0268110fa94b6bc164a0f5e
nohyewon <yw4913@naver.com>
2024-11-18 19:57:09 +0900

    :sparkles:[feat]: 임시 로그인 로직 연결

--- App.jsx
+++ App.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabs from './src/navigation/BottomTabs';
+import LoginSignupStack from './src/navigation/stack/LoginSignupStack';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      // 임시 연결 true: 로그인 완료(홈화면으로) false: 로그인 미완료(Start 화면으로)
+      setIsAuthenticated(true);
+    };
+    checkAuthStatus();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="MainTabs" component={BottomTabs} />
+        {isAuthenticated ? (
+          <Stack.Screen name="MainTabs" component={BottomTabs} />
+        ) : (
+          <Stack.Screen name="LoginSignup" component={LoginSignupStack} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
