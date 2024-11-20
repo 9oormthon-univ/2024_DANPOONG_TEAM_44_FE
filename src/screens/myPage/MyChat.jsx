a5884dfa5e5b964c1995ec30a149c378063b11ae
nohyewon <yw4913@naver.com>
2024-11-20 23:43:08 +0900

    :sparkles:[feat]: 내 채팅 하단바 없애기

--- src/screens/myPage/MyChat.jsx
+++ src/screens/myPage/MyChat.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, FlatList, StyleSheet } from 'react-native';
+import useHideBottomTabs from '../../hooks/useHideBottomTabs';
 import ChatItem from '../../components/community/ChatItem';
 import { chats } from '../../constants/mockData';
 
 function MyChat() {
   const navigation = useNavigation();
+
+  useHideBottomTabs(navigation);
 
   return (
     <View style={styles.container}>
