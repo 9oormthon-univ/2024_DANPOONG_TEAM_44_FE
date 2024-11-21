651b3ead042f49f83712eda4261e59aaf9c38332
나희 <128676942+seraphicalness@users.noreply.github.com>
2024-11-21 17:49:46 +0900

    :wrench:[rename]:폴더 경로 수정

--- src/screens/houseInfo/index.js
+++ src/screens/houseInfo/index.js
@@ -1,8 +1,8 @@
 export { default as HouseInfo } from './HouseInfo';
 export { default as FindAgent } from './FindAgent';
 export { default as MarketPriceCheck } from './MarketPriceCheck';
-export { default as Apartment } from './Apartment';
-export { default as MultiFamily } from './MultiFamily';
-export { default as OfficeTel } from './OfficeTel';
+export { default as Apartment } from './apt/Apartment';
+export { default as MultiFamily } from './mulitifamily/MultiFamily';
+export { default as OfficeTel } from './officetel/OfficeTel';
 export { default as RentCalculate } from './RentCalculate';
 export { default as CalculateResult } from './CalculateResult';
