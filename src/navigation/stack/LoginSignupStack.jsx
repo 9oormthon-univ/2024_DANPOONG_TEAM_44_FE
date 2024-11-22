c649e33a09e99554f47ecbbdf5d2a0b1a86d0efa
나희 <128676942+seraphicalness@users.noreply.github.com>
2024-11-22 22:15:42 +0900

    :sparkles:[feat]: 로그인 스택 추가

--- src/navigation/stack/LoginSignupStack.jsx
+++ src/navigation/stack/LoginSignupStack.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Start from '../../screens/Start';
-import Login from '../../screens/login/Login';
 import { Signup, SignupArea, SignupNickname } from '../../screens/signup/index';
 import { BackIcon } from '../../assets/icons/iconSvg';
+import Login from '../../screens/login/Login';
+import KakaoLogin from '../../screens/login/KakaoLogin';
 
 const Stack = createNativeStackNavigator();
 
@@ -29,6 +30,18 @@ function LoginSignupStack() {
       <Stack.Screen
         name="Login"
         component={Login}
+        options={({ navigation }) => ({
+          title: '',
+          headerLeft: () => (
+            <View style={{ marginLeft: 10 }}>
+              <BackIcon onPress={() => navigation.goBack()} />
+            </View>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="KakaoLogin"
+        component={KakaoLogin}
         options={({ navigation }) => ({
           title: '',
           headerLeft: () => (
