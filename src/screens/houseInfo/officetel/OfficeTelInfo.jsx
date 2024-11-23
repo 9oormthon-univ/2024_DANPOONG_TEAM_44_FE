3df09a06f97c4fc5b45209250e0e78af2302f78c
나희 <128676942+seraphicalness@users.noreply.github.com>
2024-11-24 00:11:36 +0900

    :sparkles:[feat]: 오피스텔 전월세 시세 조회 API(카카오미니맵제외)

--- src/screens/houseInfo/officetel/OfficeTelInfo.jsx
+++ src/screens/houseInfo/officetel/OfficeTelInfo.jsx
@@ -14,28 +14,35 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 
 const OfficeTelInfo = () => {
   const navigation = useNavigation();
-  const [year, setYear] = useState('');
-  const [mainNumber, setMainNumber] = useState('');
-  const [subNumber, setSubNumber] = useState('');
-  const [buildingName, setBuildingName] = useState('');
+  const [rcptYr, setYear] = useState('');
+  const [mno, setMainNumber] = useState('');
+  const [sno, setSubNumber] = useState('');
   const route = useRoute();
   const { city, district, neighborhood } = route.params; // 이전 화면에서 전달된 데이터
 
   const handleSearch = () => {
-    if (!year || !mainNumber || !subNumber || !buildingName) {
+    if (!rcptYr || !mno || !sno) {
       alert('모든 항목을 입력해주세요.');
       return;
     }
 
+    console.log('입력된 데이터info:', {
+      city,
+      rcptyr: rcptYr,
+      cggnm: district,
+      stdgcd: neighborhood,
+      mno: mno,
+      sno: sno,
+    });
+
     // 모든 데이터를 함께 전달
-    navigation.navigate('OfficeTelResult', {
+    navigation.navigate('ApartmentResult', {
       city,
       district,
       neighborhood,
-      year,
-      mainNumber,
-      subNumber,
-      buildingName,
+      year: rcptYr,
+      mainNumber: mno,
+      subNumber: sno,
     });
   };
 
@@ -50,7 +57,7 @@ const OfficeTelInfo = () => {
           keyboardShouldPersistTaps="handled"
         >
           <View style={styles.header}>
-            <Text style={styles.title}>오피스텔 시세</Text>
+            <Text style={styles.title}>다세대 시세</Text>
           </View>
 
           <View style={styles.inputContainer}>
@@ -59,7 +66,7 @@ const OfficeTelInfo = () => {
               style={styles.input}
               placeholder="접수 연도"
               placeholderTextColor="#B0B0B0"
-              value={year}
+              value={rcptYr}
               onChangeText={setYear}
               keyboardType="numeric"
             />
@@ -69,7 +76,7 @@ const OfficeTelInfo = () => {
               style={styles.input}
               placeholder="본번"
               placeholderTextColor="#B0B0B0"
-              value={mainNumber}
+              value={mno}
               onChangeText={setMainNumber}
               keyboardType="numeric"
             />
@@ -79,18 +86,9 @@ const OfficeTelInfo = () => {
               style={styles.input}
               placeholder="부번"
               placeholderTextColor="#B0B0B0"
-              value={subNumber}
+              value={sno}
               onChangeText={setSubNumber}
               keyboardType="numeric"
-            />
-
-            {/* 건물명 */}
-            <TextInput
-              style={styles.input}
-              placeholder="건물명"
-              placeholderTextColor="#B0B0B0"
-              value={buildingName}
-              onChangeText={setBuildingName}
             />
           </View>
 
