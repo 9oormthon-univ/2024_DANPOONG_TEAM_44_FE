3de0ccaa00be75ce061b9b7faadcce3640ec2ad7
nohyewon <yw4913@naver.com>
2024-11-24 02:58:20 +0900

    :sparkles:[feat]: 공통 api 연결 파일 작성

---
+++ src/services/apiService.js
@@ -0,0 +1,68 @@
+import { SERVER_URL } from '@env';
+
+const requestFetch = async (url, method, data, headers = {}) => {
+  const token =
+    'eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImxvZ2luSWQiOiIxMjM0Iiwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE3MzIzODIwNzksImV4cCI6MTczMjM4NTY3OX0.K0j7HnrP_7-c-AGgHE3iOB9YnLq06o--4IfZd4spPaE';
+
+  try {
+    const options = {
+      method,
+      headers: {
+        Accept: '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        Connection: 'keep-alive',
+        access: token,
+        ...headers,
+      },
+      ...(data && { body: JSON.stringify(data) }),
+    };
+
+    const response = await fetch(`${SERVER_URL}${url}`, options);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // PATCH 요청일 경우 text로 응답 처리
+    if (method === 'PATCH') {
+      const responseText = await response.text();
+      return responseText;
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Error in ${method} request to ${url}:`, error);
+    throw error;
+  }
+};
+
+// POST 요청
+export const requestPostFetch = async (url, data) => {
+  return await requestFetch(url, 'POST', data, {
+    'Content-Type': 'application/json',
+  });
+};
+
+// PUT 요청
+export const requestPutFetch = async (url, data) => {
+  return await requestFetch(url, 'PUT', data, {
+    'Content-Type': 'application/json',
+  });
+};
+
+// GET 요청
+export const requestGetFetch = async url => {
+  return await requestFetch(url, 'GET');
+};
+
+// DELETE 요청
+export const requestDeleteFetch = async url => {
+  return await requestFetch(url, 'DELETE');
+};
+
+// PATCH 요청
+export const requestPatchFetch = async (url, data) => {
+  return await requestFetch(url, 'PATCH', data, {
+    'Content-Type': 'application/json',
+  });
+};
