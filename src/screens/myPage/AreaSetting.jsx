e0c821688bb78e758d40c205f93d84350429becb
nohyewon <yw4913@naver.com>
2024-11-24 02:10:01 +0900

    :sparkles:[feat]: 마이페이지 지역 업데이트 api 연결

--- src/screens/myPage/AreaSetting.jsx
+++ src/screens/myPage/AreaSetting.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   TextInput,
@@ -12,11 +12,34 @@ import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/common/Header';
 import useHideBottomTabs from '../../hooks/useHideBottomTabs';
+import { requestPatchFetch } from '../../services/apiService';
 
 function AreaSetting() {
   const navigation = useNavigation();
+  const [sido, setSido] = useState('');
+  const [sigungu, setSigungu] = useState('');
+  const [roadname, setRoadname] = useState('');
 
   useHideBottomTabs(navigation);
+
+  const handleUpdate = async () => {
+    const requestBody = { sido, sigungu, roadname };
+
+    try {
+      const response = await requestPatchFetch('/changeLocation', requestBody);
+
+      if (
+        typeof response === 'string' &&
+        response.includes('성공적으로 업데이트되었습니다')
+      ) {
+        navigation.navigate('MyPage');
+      } else {
+        console.error('업데이트 실패: 예상하지 못한 서버 응답');
+      }
+    } catch (error) {
+      console.error('지역 정보 업데이트 실패:', error);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -33,22 +56,25 @@ function AreaSetting() {
             style={styles.input}
             placeholder="광역시/도"
             placeholderTextColor="#868686"
+            value={sido}
+            onChangeText={setSido}
           />
           <TextInput
             style={styles.input}
             placeholder="시/군/구"
             placeholderTextColor="#868686"
+            value={sigungu}
+            onChangeText={setSigungu}
           />
           <TextInput
             style={styles.input}
             placeholder="도로명"
             placeholderTextColor="#868686"
+            value={roadname}
+            onChangeText={setRoadname}
           />
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('MyPage')}
-          >
+          <TouchableOpacity style={styles.button} onPress={handleUpdate}>
             <Text style={styles.buttonText}>수정하기</Text>
           </TouchableOpacity>
         </ScrollView>
